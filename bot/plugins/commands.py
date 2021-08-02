9b38333bf3795b5b690567de17d4c3774f710151
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-08-02 16:01:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -98,9 +98,11 @@ async def help(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
+    await bot.send_photo(
         chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
+        photo="https://telegra.ph//file/d38c95da9e83a8e049078.jpg",
+        caption=Translation.START_TEXT.format(
+                update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
