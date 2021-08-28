8b44c80590490ec4418f5f9109ce8cba008d63f8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-28 19:48:25 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -72,7 +72,7 @@ async def start(bot, update):
     ],[
         InlineKeyboardButton('🕯️ᴍᴀɪɴ ɢʀᴏᴜᴘ🕯️', url='https://t.me/MoviesLokamMalayalam')
     ],[
-        InlineKeyboardButton('🕯️ᴄʟᴏꜱᴇ🕯️ ⚙', callback_data="close")
+        InlineKeyboardButton('🕯️ᴄʟᴏꜱᴇ ᴍᴇɴᴜ🕯️', callback_data="close")
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
